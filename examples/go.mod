6797e50b8f5dcff4408fbfad41718999cc35e355
Menno van Rahden <menno.vrahden@gmail.com>
2024-01-03 00:42:17 +0100

    add mysuite example

--- examples/go.mod
+++ examples/go.mod
@@ -6,4 +6,17 @@ toolchain go1.21.0
 
 replace github.com/mvrahden/go-test => ./..
 
-require github.com/mvrahden/go-test v0.0.0-00010101000000-000000000000
+require (
+	github.com/mvrahden/go-test v0.0.0-00010101000000-000000000000
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/samber/lo v1.39.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/tools v0.16.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ examples/my/my_test.go
@@ -0,0 +1,86 @@
+package my_test
+
+import (
+	"github.com/mvrahden/go-test/pkg/gotest"
+)
+
+//go:generate github.com/mvrahden/go-test/cmd/testgen -skip-autogen
+
+// `-auto-gen` induces a re-generation upon each test-execution.
+
+type F_MyFocusedTestSuite struct{}
+type X_MySkippedTestSuite struct{}
+type MyNoopTestSuite struct{}
+
+type MyTestSuite struct {
+	ExecutedC chan string
+}
+
+func (s *MyTestSuite) BeforeAll(t *gotest.T) {
+	s.ExecutedC = make(chan string, 100)
+	s.ExecutedC <- "BeforeAll"
+}
+
+func (s *MyTestSuite) BeforeEach(t *gotest.T) {
+	s.ExecutedC <- "BeforeEach"
+}
+
+func (s *MyTestSuite) XTestSomethingSpecific(t *gotest.T) {
+	s.ExecutedC <- "XTestSomethingSpecific"
+} // skip
+
+func (s *MyTestSuite) FTestSomethingSpecific(t *gotest.T) {
+	s.ExecutedC <- "FTestSomethingSpecific"
+} // focus
+
+func (s *MyTestSuite) TestSomethingSpecific(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingSpecific"
+}
+
+func (s *MyTestSuite) TestSomethingFunction(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingFunction"
+}
+
+func (s *MyTestSuite) TestSomethingAny(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingAny"
+}
+
+func (s *MyTestSuite) TestSomethingTime(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingTime"
+}
+
+func (s *MyTestSuite) TestSomethingDuration(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingDuration"
+}
+
+func (s *MyTestSuite) TestSomethingAsync(t *gotest.T, done func()) {
+	go func() {
+		s.ExecutedC <- "TestSomethingAsync"
+		done()
+	}()
+}
+
+func (s *MyTestSuite) TestSomethingB(t *gotest.T) {
+	s.ExecutedC <- "TestSomethingB"
+}
+
+func (s *MyTestSuite) TestParallelSomethingC(t *gotest.T) {
+	s.ExecutedC <- "TestParallelSomethingC"
+}
+
+func (s *MyTestSuite) TestParallelSomethingD(t *gotest.T) {
+	s.ExecutedC <- "TestParallelSomethingD"
+}
+
+func (s *MyTestSuite) TestParallelSomethingE(t *gotest.T) {
+	s.ExecutedC <- "TestParallelSomethingE"
+}
+
+func (s *MyTestSuite) AfterEach(t *gotest.T) {
+	s.ExecutedC <- "AfterEach"
+}
+
+func (s *MyTestSuite) AfterAll(t *gotest.T) {
+	s.ExecutedC <- "AfterAll"
+	close(s.ExecutedC)
+}
