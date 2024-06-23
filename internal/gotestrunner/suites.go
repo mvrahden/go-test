db9fa36ef5fbe27175efccd406654ffdee300c59
Menno van Rahden <menno.vrahden@gmail.com>
2024-06-23 11:40:32 +0200

    separate execution phases

---
+++ internal/gotestrunner/suites.go
@@ -0,0 +1,38 @@
+package gotestrunner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mvrahden/go-test/about"
+	"github.com/mvrahden/go-test/internal/cmd/testgen"
+)
+
+func SuitesGenerate(scanDir string, nargs []string) (err error) {
+	result, err := testgen.GenerateSuites(scanDir)
+	if err != nil {
+		return fmt.Errorf("failed generating suites: %w", err)
+	}
+
+	if len(result.PTest) > 0 {
+		testsuiteFile := filepath.Join(result.AbsPath, about.PSuite)
+		err := os.WriteFile(testsuiteFile, result.PTest, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("failed writing ptest: %w", err)
+		}
+	}
+	if len(result.PXTest) > 0 {
+		testsuiteFile := filepath.Join(result.AbsPath, about.PXSuite)
+		err := os.WriteFile(testsuiteFile, result.PXTest, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("failed writing pxtest: %w", err)
+		}
+	}
+	return nil
+}
+
+func SuitesCleanup(pkgPath string) {
+	os.Remove(filepath.Join(pkgPath, about.PSuite))
+	os.Remove(filepath.Join(pkgPath, about.PXSuite))
+}
