57442f277af7a11d140c4060c6401033e22c1893
Menno <menno.vrahden@gmail.com>
2026-04-25 19:19:21 +0200

    feat(cli): add --ci focus guard flag

---
+++ cmd/testsuite/focusguard.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mvrahden/go-test/internal/gotestgen"
+)
+
+type FocusViolation struct {
+	SuiteName  string
+	MethodName string
+}
+
+func (v FocusViolation) String() string {
+	if v.MethodName != "" {
+		return fmt.Sprintf("  %s.%s", v.SuiteName, v.MethodName)
+	}
+	return fmt.Sprintf("  type %s", v.SuiteName)
+}
+
+func RunFocusGuard(patterns []string) ([]FocusViolation, error) {
+	var violations []FocusViolation
+	for _, pattern := range patterns {
+		suites, err := gotestgen.Collect(pattern)
+		if err != nil {
+			return nil, err
+		}
+		for _, s := range suites {
+			name := s.Identifier()
+			if strings.HasPrefix(name, "F_") {
+				violations = append(violations, FocusViolation{SuiteName: name})
+			}
+			for _, tc := range s.TestCases() {
+				tcName := tc.Identifier()
+				if strings.HasPrefix(tcName, "F_") {
+					violations = append(violations, FocusViolation{SuiteName: name, MethodName: tcName})
+				}
+			}
+		}
+	}
+	return violations, nil
+}
