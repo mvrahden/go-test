131ebeaedfc65b202d2137b9a3de95ed2f405e1f
Menno <menno.vrahden@gmail.com>
2026-04-25 19:16:53 +0200

    feat(gotest): add When(), Helper(), Errorf(), FailNow() to T

--- pkg/gotest/t.go
+++ pkg/gotest/t.go
@@ -17,9 +17,20 @@ type T struct {
 }
 
 func (t *T) T() *testing.T { return t.t }
+func (t *T) Helper()       { t.t.Helper() }
+func (t *T) Errorf(format string, args ...any) {
+	t.t.Helper()
+	t.t.Errorf(format, args...)
+}
+func (t *T) FailNow() { t.t.FailNow() }
 func (t *T) It(description string, testFn func(it *T)) {
 	t.t.Run(description, func(t *testing.T) {
 		testFn(NewT(t))
+	})
+}
+func (t *T) When(description string, fn func(w *T)) {
+	t.t.Run(description, func(tt *testing.T) {
+		fn(NewT(tt))
 	})
 }
 func (t *T) Assert(v any) *assert.BaseAssertionContext {
