c1963ee94d506d4eb72ce154d238cf81b97f420e
Menno <menno.vrahden@gmail.com>
2026-04-25 15:51:22 +0200

    fix: remove double-append of args in StdlibRunTests

--- internal/gotestrunner/stdlib.go
+++ internal/gotestrunner/stdlib.go
@@ -6,9 +6,6 @@ import (
 
 func StdlibRunTests(args []string) (out []byte, code int, err error) {
 	cmd := exec.Command("go", append([]string{"test"}, args...)...)
-	if len(args) > 0 {
-		cmd.Args = append(cmd.Args, args...)
-	}
 	out, _ = cmd.CombinedOutput()
 	return out, cmd.ProcessState.ExitCode(), nil
 }
