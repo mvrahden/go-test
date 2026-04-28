260ad8b820d6a21a1346cd550027faedb8a721f9
Menno <menno.vrahden@gmail.com>
2026-04-28 11:45:21 +0200

    test(scaffold): add testdata for file-scoped scaffold

---
+++ internal/scaffold/testdata/sampletype/funcs.go
@@ -0,0 +1,7 @@
+package sampletype
+
+func CalculateDiscount(amount float64, tier string) float64 { return 0 }
+
+func ApplyTax(amount float64, region string) float64 { return 0 }
+
+func internalHelper() {}
