8932254711ccf0fb2dfe08165d6c9f815b0aebde
Menno <menno.vrahden@gmail.com>
2026-04-28 11:50:22 +0200

    feat(scaffold): wire file-scoped scaffold into CLI

--- cmd/gotest/scaffold.go
+++ cmd/gotest/scaffold.go
@@ -19,7 +19,7 @@ func runScaffold(args []string) int {
 	}
 
 	if target == "" {
-		fmt.Fprintln(os.Stderr, "usage: gotest scaffold ./pkg/path.TypeName or ./path/file.go")
+		fmt.Fprintln(os.Stderr, "usage: gotest scaffold <./pkg/path.TypeName | ./pkg/path/file.go>")
 		return 1
 	}
 
@@ -27,45 +27,6 @@ func runScaffold(args []string) int {
 		return runScaffoldFile(target)
 	}
 	return runScaffoldType(target)
-}
-
-func runScaffoldFile(target string) int {
-	infos, err := scaffold.IntrospectFile(target)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "scaffold: %v\n", err)
-		return 1
-	}
-
-	for _, info := range infos {
-		filename := scaffold.ToSnakeCase(info.Name) + "_suite_test.go"
-		outPath := filepath.Join(info.PkgDir, filename)
-
-		if _, err := os.Stat(outPath); err == nil {
-			fmt.Fprintf(os.Stderr, "scaffold: %s already exists\n", outPath)
-			return 1
-		}
-
-		var out []byte
-		switch {
-		case info.IsFuncBased:
-			out, err = scaffold.GenerateFuncScaffold(info)
-		case info.IsInterface:
-			out, err = scaffold.GenerateContractScaffold(info)
-		default:
-			out, err = scaffold.GenerateScaffold(info)
-		}
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "scaffold: %v\n", err)
-			return 1
-		}
-
-		if err := os.WriteFile(outPath, out, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "scaffold: failed to write %s: %v\n", outPath, err)
-			return 1
-		}
-		writeGenerated(outPath)
-	}
-	return 0
 }
 
 func runScaffoldType(target string) int {
@@ -81,14 +42,6 @@ func runScaffoldType(target string) int {
 		return 1
 	}
 
-	filename := scaffold.ToSnakeCase(typeName) + "_suite_test.go"
-	outPath := filepath.Join(info.PkgDir, filename)
-
-	if _, err := os.Stat(outPath); err == nil {
-		fmt.Fprintf(os.Stderr, "scaffold: %s already exists\n", outPath)
-		return 1
-	}
-
 	var out []byte
 	if info.IsInterface {
 		out, err = scaffold.GenerateContractScaffold(info)
@@ -100,23 +53,47 @@ func runScaffoldType(target string) int {
 		return 1
 	}
 
-	if err := os.WriteFile(outPath, out, 0644); err != nil {
+	filename := scaffold.ToSnakeCase(typeName) + "_suite_test.go"
+	return writeScaffoldFile(info.PkgDir, filename, out)
+}
+
+func runScaffoldFile(target string) int {
+	dir := filepath.Dir(target)
+	filename := filepath.Base(target)
+	pkgPattern := "./" + filepath.ToSlash(dir)
+
+	info, err := scaffold.IntrospectFile(pkgPattern, filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "scaffold: %v\n", err)
+		return 1
+	}
+
+	out, err := scaffold.GenerateFileScaffold(info)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "scaffold: %v\n", err)
+		return 1
+	}
+
+	outFilename := scaffold.ToSnakeCase(strings.TrimSuffix(filename, ".go")) + "_suite_test.go"
+	return writeScaffoldFile(info.PkgDir, outFilename, out)
+}
+
+func writeScaffoldFile(pkgDir, filename string, content []byte) int {
+	outPath := filepath.Join(pkgDir, filename)
+
+	if err := os.WriteFile(outPath, content, 0644); err != nil {
 		fmt.Fprintf(os.Stderr, "scaffold: failed to write %s: %v\n", outPath, err)
 		return 1
 	}
-	writeGenerated(outPath)
-	return 0
-}
-
-func writeGenerated(outPath string) {
 	cwd, cwdErr := os.Getwd()
 	if cwdErr == nil {
 		if rel, err := filepath.Rel(cwd, outPath); err == nil {
 			fmt.Printf("Generated: %s\n", rel)
-			return
+			return 0
 		}
 	}
 	fmt.Printf("Generated: %s\n", outPath)
+	return 0
 }
 
 func isFlag(s string) bool {
