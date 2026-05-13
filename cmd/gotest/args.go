88bf197a1789482a0c9e7d650022a1fb028ce454
Menno <menno.vrahden@gmail.com>
2026-05-13 11:47:32 +0200

    refactor(gotest): deduplicate looksLikePackagePattern into gotestrunner

--- cmd/gotest/args.go
+++ cmd/gotest/args.go
@@ -3,6 +3,8 @@ package main
 import (
 	"strings"
 	"time"
+
+	"github.com/mvrahden/go-test/internal/gotestrunner"
 )
 
 type ExecConfig struct {
@@ -88,7 +90,7 @@ func ExtractPackagePatterns(goTestArgs []string) []string {
 		if strings.HasPrefix(arg, "-") {
 			continue
 		}
-		if looksLikePackagePattern(arg) {
+		if gotestrunner.LooksLikePackagePattern(arg) {
 			patterns = append(patterns, arg)
 		}
 	}
@@ -96,10 +98,6 @@ func ExtractPackagePatterns(goTestArgs []string) []string {
 		return []string{"."}
 	}
 	return patterns
-}
-
-func looksLikePackagePattern(s string) bool {
-	return strings.HasPrefix(s, ".") || strings.HasPrefix(s, "/") || strings.Contains(s, "/")
 }
 
 func extractTagsFlag(args []string) (tags string, remaining []string) {
